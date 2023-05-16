78cb4eedd8bfb0d8d3b4e849ae7d2840a7e77d4a
Hemerson Ballen <hjballent@gmail.com>
2023-05-16 15:09:16 -0500

    Creating Script

---
+++ formulariojs/app.js
@@ -0,0 +1,10 @@
+const d = document,
+  $body = d.querySelector("body");
+d.addEventListener("submit", (e) => {
+  if (e.target.matches("#")) {
+    const $form = d.getElementById("");
+
+    if ($form.user === "admin" && $form.password === "admin") {
+    }
+  }
+});
